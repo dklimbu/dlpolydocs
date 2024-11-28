5ec1f089570dfe16f670d4c28601446ac0b571b9
Dil Limbu <dlimbu197@gmail.com>
2024-11-28 09:10:13 -0600

    h-CMD tutorials update

---
+++ source/tutorials/IBI_code.py
@@ -0,0 +1,41 @@
+npair=15
+cutoff=10.0
+nrdf=480
+pairtype=["C OW",
+        "C HW",
+        "N OW",
+        "N HW",
+        "O OW",
+        "O HW",
+        "F OW",
+        "F HW",
+        "S OW",
+        "S HW",
+        "Li OW",
+        "Li HW",
+        "OW OW",
+        "OW HW",
+        "HW HW"
+          ]
+nmols=2
+nbonds=[4,1]
+totbonds=sum(nbonds)
+bonds=[
+        ["harm",4],
+        ["harm",6],
+        ["harm",2],
+        ["harm",2],
+        ["quar",2]
+        ]
+nangs=[7,1]
+totangs=sum(nangs)
+angs=[
+        ["harm",4],
+        ["harm",2],
+        ["harm",4],
+        ["harm",6],
+        ["harm",6],
+        ["harm",2],
+        ["harm",1],
+        ["harm",1],
+        ]
